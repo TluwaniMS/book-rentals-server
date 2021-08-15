16e39f869ed5759c84dc47dfe64813cfc27c7408
tluwaniMS <tluwani2020@gmail.com>
2021-08-15 16:32:43 +0200

    adding-genres-sample-data

---
+++ sample-data/genres.js
@@ -0,0 +1,14 @@
+const genres = [
+  { genre: "Action and Adventure" },
+  { genre: "Classics" },
+  { genre: "Fantasy" },
+  { genre: "Detective and Mystery" },
+  { genre: "Historical Fiction" },
+  { genre: "Horror" },
+  { genre: "Essays" },
+  { genre: "Romance" },
+  { genre: "Memoir" },
+  { genre: "History" }
+];
+
+module.exports = { genres };
