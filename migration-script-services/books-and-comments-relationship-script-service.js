f9a00179608e6132e82dc792bd4da4ef7b46c2fe
tluwaniMS <tluwani2020@gmail.com>
2021-08-17 23:13:10 +0200

    adding-books-and-comments-service-scripts

---
+++ migration-script-services/books-and-comments-relationship-script-service.js
@@ -0,0 +1,24 @@
+const { Book } = require("../models/book");
+const {
+  booksAndCommentsIdLinks
+} = require("../sample-data/books-and-comments-id-links");
+const { comments } = require("../sample-data/comments");
+
+async function linkCommentsToBooks() {
+  booksAndCommentsIdLinks.forEach(async (link) => {
+    await dbQueryRunnerForLinkingCommentsToBooks(link.bookId, link.commentIds);
+  });
+}
+
+async function dbQueryRunnerForLinkingCommentsToBooks(bookId, commentIds) {
+  commentIds.forEach(async (commentId) => {
+    let comment = extractCommentById(commentId);
+    await Book.updateOne({ _id: bookId }, { $push: { comments: comment } });
+  });
+}
+
+function extractCommentById(commentId) {
+  const comment = comments.filter((review) => review.id === commentId)[0];
+
+  return comment;
+}
