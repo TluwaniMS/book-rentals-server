e623421a13a320aca612eb42490ebb8a131c2d20
tluwaniMS <tluwani2020@gmail.com>
2021-08-19 22:17:08 +0200

    adding-logic-to-rented-books-route-end-points

--- routes/rented-books.js
+++ routes/rented-books.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const { errorHandler } = require("../middleware/error-handler");
+const { RentedBook } = require("../models/rented-book");
 
 router.get(
   "/get-books-rented-by-user/:userId",
-  errorHandler(async (req, res) => {})
+  errorHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    const booksRentedByUser = await RentedBook.find({
+      user: { $contains: userId }
+    });
+
+    res.status(200).send({ data: booksRentedByUser });
+  })
 );
 
 router.get(
   "/get-all-books-rented",
-  errorHandler(async (req, res) => {})
+  errorHandler(async (req, res) => {
+    const rentedBooks = await RentedBook.find({})
+      .populate("book")
+      .populate("user");
+
+    res.status(200).send({ data: rentedBooks });
+  })
 );
 
 module.exports = router;
